b11b31f99920607bfdaefaa10cbf4ba1c503e977
Thomas Quandalle <cedric@verstraeten.io>
2022-05-13 11:25:29 +0200

    one test

--- exercise-1/test/converter.js
+++ exercise-1/test/converter.js
@@ -5,6 +5,11 @@ describe("Color Code Converter", function() {
 
     describe("RGB to Hex conversion", function() {
 
-      
+        // specification for RGB to HEX converter   
+        it("converts the basic colors", function() {
+            var whiteHex   = converter.rgbToHex(255, 255, 255);
+            expect(whiteHex).to.equal("ffffff");
+        });
+
     });
 });
