6bc35ee5e39d590947a44dc463ef9419303cfcd8
Thomas Quandalle <cedric@verstraeten.io>
2022-05-13 10:16:40 +0200

    fix

--- exercise-1/test/converter.js
+++ exercise-1/test/converter.js
@@ -3,27 +3,4 @@ var converter = require("../app/converter");
 
 describe("Color Code Converter", function() {
 
-    describe("RGB to Hex conversion", function() {
-        // specification for RGB to HEX converter   
-        it("converts the basic colors", function() {
-            var redHex   = converter.rgbToHex(255, 0, 0);
-            var greenHex = converter.rgbToHex(0, 255, 0);
-            var blueHex  = converter.rgbToHex(0, 0, 255);
-
-            expect(redHex).to.equal("ff0000");
-            expect(greenHex).to.equal("00ff00");
-            expect(blueHex).to.equal("0000ff");
-        });
-
-        // specification for RGB to HEX converter   
-        it("converts the basic colors2", function() {
-            var redHex   = converter.rgbToHex(255, 128, 0);
-            var greenHex = converter.rgbToHex(128, 255, 0);
-            var blueHex  = converter.rgbToHex(128, 0, 255);
-
-            expect(redHex).to.equal("ff8000");
-            expect(greenHex).to.equal("80ff00");
-            expect(blueHex).to.equal("8000ff");
-        });
-    });
 });
