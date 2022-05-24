fed36da443b840737fa64759ae91bf46a74881b4
Azure For Everyone <105457388+azureforeveryone@users.noreply.github.com>
2022-05-24 16:03:29 +0200

    robert

--- exercise-5/app/palindrome.js
+++ exercise-5/app/palindrome.js
@@ -2,9 +2,10 @@
 
 exports.isPalindrome = function(word) {
     var len = word.length;
+    console.wr--
     var mid = Math.floor(len/2);
     for ( var i = 0; i < mid; i++ ) {
-        if (word[i] !== word[len - i]) {
+        if (word[i] !== word[len - 1 - i]) {
             return false;
         }
     }
