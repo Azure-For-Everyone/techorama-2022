6ccbd43fe6c8222bc8a0e0ffa6fdcd6df452a504
Azure For Everyone <105457388+azureforeveryone@users.noreply.github.com>
2022-05-24 14:49:23 +0200

    solution

--- exercise-6/app/message.js
+++ exercise-6/app/message.js
@@ -1,9 +1,9 @@
 // app/message.js
 
 exports.countCharacters = function(word) {
-    let length = 0;
+    var length = 0;
     for (let i = 0; i < word.length; i++) {
-        length = length
+        length++;
     }
     return length;
 };
