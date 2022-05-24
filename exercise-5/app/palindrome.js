6fe1010f85d9b11b5e436dff590120d752384291
azureforeveryone <cverstraeten@microsoft.com>
2022-05-23 23:02:21 -0700

    earlymorningfix

--- exercise-5/app/palindrome.js
+++ exercise-5/app/palindrome.js
@@ -3,8 +3,8 @@
 exports.isPalindrome = function(word) {
     var len = word.length;
     var mid = Math.floor(len/2);
-    for ( var i = 0; i < mid; i++ ) {
-        if (word[i] !== word[len - i]) {
+    for ( var i = 0; i <= mid; i++ ) {
+        if (word[i] !== word[len - i - 1]) {
             return false;
         }
     }
