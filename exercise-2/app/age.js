c98cec05750710cfb4e55b9942bb59cd82b1dca1
Azure For Everyone <105457388+azureforeveryone@users.noreply.github.com>
2022-05-25 16:08:21 +0200

    fix

--- exercise-2/app/age.js
+++ exercise-2/app/age.js
@@ -1,5 +1,12 @@
 // app/age.js
 
 exports.ageToSeconds = function(years) {
-    return 60 * 24 * 365 * years
+  //  var leapYears = years / 4 + 1;
+  //  return Calculate(leapYears, 366) + Calculate(years - leapYears - 1, 365);
+
+  return Calculate(years, 365);
 };
+
+function Calculate(years, days){
+    return years * 24 * 60 * 60 * days;
+}
