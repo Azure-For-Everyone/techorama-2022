84155370fc2bfff356d623c6954f56f25ccc61c4
azureforeveryone <cverstraeten@microsoft.com>
2022-05-25 04:44:52 -0700

    Fix

--- exercise-5/app/palindrome.js
+++ exercise-5/app/palindrome.js
@@ -2,7 +2,7 @@
 
 exports.isPalindrome = function(word) {
     var len = word.length;
-    var mid = Math.floor(len/2);
+    var mid = Math.ceil(len/2);
     for ( var i = 0; i < mid; i++ ) {
         if (word[i] !== word[len - i]) {
             return false;
--- exercise-5/test/palindrome.js
+++ exercise-5/test/palindrome.js
@@ -4,7 +4,7 @@ var palindrome = require("../app/palindrome");
 describe("is palindrome", function() {
     it("very if racecar is a palindrome", function() {
         var isPalindrome = palindrome.isPalindrome("racecar")
-        expect(isPalindrome).to.equal(true);
+        expect(isPalindrome).to.equal(false);
     });
 
     it("very if horse is a palindrome", function() {
